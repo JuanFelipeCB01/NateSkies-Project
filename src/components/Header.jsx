cbc821877de59f4aa72c26e3019ecd1f43bd7840
JuanFelipeCB01 <jfelipecbermudez@gmail.com>
2023-11-14 00:08:11 +0000

    header layout done and logo placed

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <h2 className='header-logo'>NATE SKIES</h2>
+      <nav className='header-nav'>
+        <p className='header-nav-text'>CLIPS</p>
+        <p className='header-nav-text'>MUSIC</p>
+        <p className='header-nav-text'>BIO</p>
+        <p className='header-nav-text'>MERCH</p>
+      </nav>
+    </div>
+  )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import natelogo from '../media/natelogo.png';
+
+export default function HomePage() {
+  return (
+    <div className='home'>
+        <section className='home-header'>
+            <img className='home-header-logo' src={natelogo} alt='logo'/>
+        </section>
+        <section className='home-folders'>
+            
+        </section>
+        
+    </div>
+  )
+}
